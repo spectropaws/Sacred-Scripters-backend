2d3ea24a978fecbc985bff97927667ccac071134
mehul515 <mehulkohad@gmail.com>
2024-03-05 03:06:53 +0530

    little changes

---
+++ models/game.model.js
@@ -0,0 +1,41 @@
+
+import mongoose from "mongoose";
+
+const gameSchema = new mongoose.Schema({
+
+    name : {
+        type:String,
+        required: true,
+    },
+    
+    price : {
+        type:Number,
+        required: true,
+        default:0,
+    },
+
+    poster : {
+        type:String,
+        required: true,
+        default:"",
+    },
+    
+    external: {
+        type: Boolean,
+        default:false,
+    },
+
+    category: {
+        type:String,
+    },
+
+    description: {
+        type:String,
+    },
+
+
+}, {timestamps: true});
+
+const Game = mongoose.model("Game", gameSchema);
+
+export default Game;
